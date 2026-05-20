a80d5feb5f6ea6b1ed0dce24bc99c600d787b947
Sushmit panday Panday <pandaysushmitpanday@gmail.com>
2026-05-20 15:56:05 +0530

    sushmit

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -11,7 +11,7 @@ export default function LoginPage() {
   // 🛠️ CHANGED: Admin dashboard ki tarah yahan local aur live render URL ka logic add kiya hai
   const API_BASE_URL = window.location.hostname === "localhost" 
     ? "http://localhost:5000" 
-    : "https://mernrenovate-2.onrender.com";
+    : "https://mernrenovate-3.onrender.com";
 
   async function handleSubmit(e) {
     e.preventDefault();

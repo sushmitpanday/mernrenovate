090fd598844cb2ea85cf7b440fbe12d41da0c6a6
Sushmit panday Panday <pandaysushmitpanday@gmail.com>
2026-05-20 18:02:58 +0530

    last

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -16,7 +16,7 @@ export default function RegisterPage() {
   // 🛠️ CHANGED: Register page par bhi localhost aur render link manage karne ka logic add kiya hai
   const API_BASE_URL = window.location.hostname === "localhost" 
     ? "http://localhost:5000" 
-    : "https://mernrenovate-3.onrender.com";
+    : "https://mernrenovate-4.onrender.com";
 
   async function handleSubmit(e) {
     e.preventDefault();

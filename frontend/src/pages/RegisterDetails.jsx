aaedb4f5d297424940c25495033d1c00ea40ec39
Sushmit panday Panday <pandaysushmitpanday@gmail.com>
2026-06-03 19:21:57 +0530

    api

--- frontend/src/pages/RegisterDetails.jsx
+++ frontend/src/pages/RegisterDetails.jsx
@@ -4,7 +4,7 @@ import { useNavigate, useLocation } from 'react-router-dom';
 
 const API_BASE_URL = window.location.hostname === "localhost" 
     ? "http://localhost:5000" 
-    : "https://mernrenovate-18.onrender.com";
+    : "https://mernrenovate-19.onrender.com";
 
 const api = axios.create({ baseURL: API_BASE_URL });
 
